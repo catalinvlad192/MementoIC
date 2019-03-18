3352104c05778b0b27731e1f2c68e3f2f4f377f5
catalinvlad192 <vlad_catalin1998@yahoo.com>
2019-03-18 21:42:11 +0200

    Added DataBase to project

---
+++ MementoIC/MementoIC/Resources/Task.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+public class Task
+{
+    private string name;
+    private string description;
+    private Boolean status;
+    private DateTime deadline;
+    private int priority;
+
+    //constructor with arguments
+    public Task(string name, string description, Boolean status, DateTime deadline, int priority)
+    {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.deadline = deadline;
+        this.priority = priority;
+    }
+
+    //constructor without arguments
+    public Task()
+    {
+        this.name = "";
+        this.description = "";
+        this.status = false;
+        this.deadline = new DateTime(2000, 1, 2, 1, 0, 0);
+        this.priority = 1;
+    }
+
+//GETTERS
+    public string getName()
+    {
+        return this.name;
+    }
+    public string getDescription()
+    {
+        return this.description;
+    }
+    public Boolean getStatus()
+    {
+        return this.status;
+    }
+    public DateTime getDeadline()
+    {
+        return this.deadline;
+    }
+    public int getPriority()
+    {
+        return this.priority;
+    }
+
+//SETTERS
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+    public void setDescription(string descr)
+    {
+        this.description = descr; ;
+    }
+    public void setStatus(Boolean status)
+    {
+        this.status = status;
+    }
+    public void setDeadline(DateTime dt)
+    {
+        this.deadline = dt;
+    }
+    public void setPriority(int pr)
+    {
+        this.priority = pr;
+    }
+}
---
+++ MementoIC/MementoIC/Resources/TaskDatabase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using SQLite;
+using System.IO;
+
+//DataBase
+public class TaskDatabase : SQLiteConnection
+{
+    static object locker = new object();
+
+    public static string DatabaseFilePath
+    {
+        get
+        {
+            var sqliteFilename = "StockDB.db3";
+            string libraryPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var path = Path.Combine(libraryPath, sqliteFilename);
+            return path;
+        }
+    }
+
+    public TaskDatabase(string path) : base(path)
+    {
+        // create the tables
+        CreateTable<Task>();
+    }
+
+    public IEnumerable<Task> GetTasks()
+    {
+        lock (locker)
+        {
+            return (from i in Table<Task>() select i).ToList();
+        }
+    }
+
+    public Task GetTask(string name)
+    {
+        lock (locker)
+        {
+            return Table<Task>().FirstOrDefault(x => x.getName() == name);
+        }
+    }
+
+    public int UpdateTask(Task item)
+    {
+        lock (locker)
+        {
+            return Update(item);
+        }
+    }
+    public int SaveTask(Task item)
+    {
+        lock (locker)
+        {
+
+            return Insert(item);
+        }
+    }
+
+    public int DeleteTask(Task tk)
+    {
+        lock (locker)
+        {   
+            return Delete<Task>(tk.getName());
+        }
+    }
+}
