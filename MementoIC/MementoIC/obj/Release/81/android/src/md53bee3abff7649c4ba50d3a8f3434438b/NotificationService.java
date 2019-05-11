c9d3dd3e3d1d7d11f3ba6a1cb3fedaa0dbad531b
catalinvlad192 <vlad_catalin1998@yahoo.com>
2019-05-11 21:24:01 +0200

    Added Doc and Spec files. Added UITests

---
+++ MementoIC/MementoIC/obj/Release/81/android/src/md53bee3abff7649c4ba50d3a8f3434438b/NotificationService.java
@@ -0,0 +1,74 @@
+package md53bee3abff7649c4ba50d3a8f3434438b;
+
+
+public class NotificationService
+	extends android.app.Service
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:()V:GetOnCreateHandler\n" +
+			"n_onStartCommand:(Landroid/content/Intent;II)I:GetOnStartCommand_Landroid_content_Intent_IIHandler\n" +
+			"n_onBind:(Landroid/content/Intent;)Landroid/os/IBinder;:GetOnBind_Landroid_content_Intent_Handler\n" +
+			"n_onDestroy:()V:GetOnDestroyHandler\n" +
+			"";
+		mono.android.Runtime.register ("TaskManager.NotificationService, MementoIC", NotificationService.class, __md_methods);
+	}
+
+
+	public NotificationService ()
+	{
+		super ();
+		if (getClass () == NotificationService.class)
+			mono.android.TypeManager.Activate ("TaskManager.NotificationService, MementoIC", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate ()
+	{
+		n_onCreate ();
+	}
+
+	private native void n_onCreate ();
+
+
+	public int onStartCommand (android.content.Intent p0, int p1, int p2)
+	{
+		return n_onStartCommand (p0, p1, p2);
+	}
+
+	private native int n_onStartCommand (android.content.Intent p0, int p1, int p2);
+
+
+	public android.os.IBinder onBind (android.content.Intent p0)
+	{
+		return n_onBind (p0);
+	}
+
+	private native android.os.IBinder n_onBind (android.content.Intent p0);
+
+
+	public void onDestroy ()
+	{
+		n_onDestroy ();
+	}
+
+	private native void n_onDestroy ();
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ MementoIC/UITest/Tests.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest
+{
+    [TestFixture(Platform.Android)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AddNewHighPriorityTask()
+        {
+            app.Tap(c => c.Id("buttonNew"));
+            app.Tap(c => c.Id("editName"));
+            app.EnterText("Test Memento");
+            app.Tap(c => c.Id("editDescription"));
+            app.EnterText("Description for the test");
+            app.DismissKeyboard();
+            app.TapCoordinates(915, 1500);
+            app.Tap(c => c.Id("buttonSave"));
+
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void AddATaskThenDeleteTheFirstTask()
+        {
+            app.Tap(c => c.Id("buttonNew"));
+            app.Tap(c => c.Id("editName"));
+            app.EnterText("Test Memento");
+            app.Tap(c => c.Id("editDescription"));
+            app.EnterText("Description for the test");
+            app.DismissKeyboard();
+            app.TapCoordinates(915, 1500);
+            app.Tap(c => c.Id("buttonSave"));
+
+            app.TapCoordinates(540, 400);
+            app.Tap(c => c.Id("editDescription"));
+            app.EnterText("");
+            app.DismissKeyboard();
+            app.Tap(c => c.Id("buttonDelete"));
+
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void Add5TasksThenSortThem()
+        {
+            app.Tap(c => c.Id("buttonNew"));
+            app.Tap(c => c.Id("editName"));
+            app.EnterText("Test 1");
+            app.DismissKeyboard();
+            app.Tap(c => c.Id("buttonSave"));
+
+            app.Tap(c => c.Id("buttonNew"));
+            app.Tap(c => c.Id("editName"));
+            app.EnterText("Test 2");
+            app.DismissKeyboard();
+            app.TapCoordinates(915, 1500);
+            app.TapCoordinates(915, 1100);
+            app.Tap(c => c.Id("buttonSave"));
+
+            app.Tap(c => c.Id("buttonNew"));
+            app.Tap(c => c.Id("editName"));
+            app.EnterText("Test 3");
+            app.DismissKeyboard();
+            app.Tap(c => c.Id("buttonSave"));
+
+            app.Tap(c => c.Id("buttonNew"));
+            app.Tap(c => c.Id("editName"));
+            app.EnterText("Test 4");
+            app.DismissKeyboard();
+            app.TapCoordinates(915, 1400);
+            app.TapCoordinates(700, 1400);
+            app.Tap(c => c.Id("buttonSave"));
+
+            app.Tap(c => c.Id("buttonNew"));
+            app.Tap(c => c.Id("editName"));
+            app.EnterText("Test 5");
+            app.DismissKeyboard();
+            app.TapCoordinates(915, 1500);
+            app.Tap(c => c.Id("buttonSave"));
+
+            app.Tap(c => c.Id("sortPriority"));
+            app.Tap(c => c.Id("sortDate"));
+
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void AddTaskThenDisableItsNotification()
+        {
+            app.Tap(c => c.Id("buttonNew"));
+            app.Tap(c => c.Id("editName"));
+            app.EnterText("Test Memento");
+            app.Tap(c => c.Id("editDescription"));
+            app.EnterText("Description for the test");
+            app.DismissKeyboard();
+            app.TapCoordinates(915, 1500);
+            app.Tap(c => c.Id("buttonSave"));
+
+            app.TapCoordinates(5, 400);
+
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void CheckDatePicker()
+        {
+            app.Tap(c => c.Id("buttonNew"));
+            app.Tap(c => c.Id("editName"));
+            app.EnterText("Test Memento");
+            app.Tap(c => c.Id("editDescription"));
+            app.EnterText("Description for the test");
+            app.DismissKeyboard();
+            app.TapCoordinates(915, 1100);
+            app.Tap(c => c.Id("buttonSave"));
+
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void CheckTimePicker()
+        {
+            app.Tap(c => c.Id("buttonNew"));
+            app.Tap(c => c.Id("editName"));
+            app.EnterText("Test Memento");
+            app.Tap(c => c.Id("editDescription"));
+            app.EnterText("Description for the test");
+            app.DismissKeyboard();
+            app.TapCoordinates(700, 1400);
+            app.Tap(c => c.Id("buttonSave"));
+
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void AddATaskThenEditIt()
+        {
+            app.Tap(c => c.Id("buttonNew"));
+            app.Tap(c => c.Id("editName"));
+            app.EnterText("Test Memento");
+            app.DismissKeyboard();
+            app.TapCoordinates(915, 1500);
+            app.Tap(c => c.Id("buttonSave"));
+
+            app.TapCoordinates(540, 400);
+            app.Tap(c => c.Id("editDescription"));
+            app.EnterText("Description for the test");
+            app.DismissKeyboard();
+            app.Tap(c => c.Id("buttonSave"));
+
+            Assert.IsTrue(true);
+        }
+    }
+}
